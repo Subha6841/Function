30a58e6af472de6615ad9a480aa16d1be7b64696
Subha6841 <84672927+Subha6841@users.noreply.github.com>
2021-05-24 22:16:26 +0530

    I am updating

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,10 @@
 
 # program to check whether a word is palindrome or not
+# coder: Subhashis Ghosh
 def palindrome(x):
+    '''
+    This function returns whether word is palindrome or not.
+    '''
     n1=x.replace(' ','')
     n2=n1[::-1]
     if n1==n2:
